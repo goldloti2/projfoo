c09d5c7d5e3d4fa036eb6d154bb3ad80a4c9abf0
goldloti2 <goldloti2@gmail.com>
2018-06-26 16:36:23 +0800

    c.c add 4 git commands, d.c add

--- c.c
+++ c.c
@@ -1,2 +1,8 @@
 git init	//initialize current folder as git project
 git add .	//add all items in the folder to staging area and ready to be the formal version
+git commit	//commit items in the satging area as the formal version
+	-m	//leave a 1-line message
+	-e	//leave a complicated message with editer
+git log		//check the commit log
+git status	//check the current modified and stage status
+git reset HEAD <filename>	//remove the file from staging area
